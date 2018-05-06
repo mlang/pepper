bd9c27bed59d7a9bf453c4ee49709f16b837fb2c
Mario Lang <mlang@delysid.org>
2018-05-06 02:14:25 +0200

    read analog ins

--- render.cpp
+++ render.cpp
@@ -5,11 +5,12 @@
 #include <lilv/lilvmm.hpp>
 #include <lv2/lv2plug.in/ns/ext/buf-size/buf-size.h>
 
+static LV2_Feature hardRTCapable = { LV2_CORE__hardRTCapable, NULL };
 static LV2_Feature buf_size_features[1] = {
   { LV2_BUF_SIZE__fixedBlockLength, NULL },
 };
-LV2_Feature* features[2] = {
-  &buf_size_features[0], nullptr
+LV2_Feature* features[3] = {
+  &hardRTCapable, &buf_size_features[0], nullptr
 };
 
 class Plugin {
@@ -50,12 +51,32 @@ public:
       port[0][frame] = audioRead(bela, frame, 0);
       port[1][frame] = audioRead(bela, frame, 1);
     }
+    { 
+      float analog[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+      for (int frame = 0; frame < bela->analogFrames; frame++) {
+        for (int channel = 0; channel < bela->analogInChannels; channel++) {
+          analog[channel] += analogRead(bela, frame, channel);
+        }
+      }
+      for (int channel = 0; channel < bela->analogInChannels; channel++) {
+        analog[channel] /= bela->analogFrames;
+      }
+      analogIn(3, analog[0]); // Pitch Factor
+      analogIn(4, analog[1]); // Robotize/Whisperize
+      analogIn(8, analog[2]); // Threshold
+      analogIn(9, analog[3]); // Attack
+      analogIn(10, analog[4]); // Transpose
+    }
     instance->run(bela->audioFrames);
     for (int frame = 0; frame < bela->audioFrames; frame++) {
       auto const sample = port[2][frame];
       audioWrite(bela, frame, 0, sample);
       audioWrite(bela, frame, 1, sample);
     }
+  }
+private:
+  void analogIn(unsigned p, float v) {
+    port[p].front() = map(v, 0, 1, minValue[p], maxValue[p]);
   }
 };
 
@@ -79,9 +100,7 @@ public:
     if (!this->plugins.empty()) index = 0;
     for (auto &plugin: plugins) plugin->activate();
   }
-  ~pepper() {
-    for (auto &plugin: plugins) plugin->deactivate();
-  }
+  ~pepper() { for (auto &plugin: plugins) plugin->deactivate(); }
   void run(BelaContext *bela) {
     if (index >= 0 && index < plugins.size()) {
       plugins[index]->run(bela);
@@ -106,4 +125,3 @@ void cleanup(BelaContext *context, void *userData)
 {
   p = nullptr;
 }
-
