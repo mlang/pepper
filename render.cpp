f631eddaa018028cdd318d202321f152a226cd83
Mario Lang <mlang@delysid.org>
2018-05-26 16:24:38 +0200

    auxtask wrapping while I wait for real hardware

--- render.cpp
+++ render.cpp
@@ -1,10 +1,30 @@
 #include <Bela.h>
+#include <AuxTaskNonRT.h>
 #include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <memory>
 #include <vector>
 #include <lilv/lilvmm.hpp>
 #include <lv2/lv2plug.in/ns/ext/buf-size/buf-size.h>
+
+enum TaskKind { RT, NonRT };
+
+template<enum TaskKind, typename Class> class MemFun;
+template<typename Class> class MemFun<NonRT, Class> : AuxTaskNonRT {
+  void (Class::* const member_function)();
+  Class * const instance;
+  static void call(void *ptr, int size) {
+    auto instance = static_cast<MemFun *>(ptr);
+    ((instance->instance)->*(instance->member_function))();
+  }
+public:
+  MemFun(const char *name, void (Class::*callback)(), Class *instance)
+  : AuxTaskNonRT(), member_function(callback), instance(instance) {
+    create(name, call);
+  }
+  void operator()() { schedule(this, sizeof(Class)); }
+};
 
 static LV2_Feature hardRTCapable = { LV2_CORE__hardRTCapable, NULL };
 static LV2_Feature buf_size_features[1] = {
@@ -35,15 +55,14 @@ public:
     static LilvNode *lv2_core__sampleRate = lilv.new_uri(LV2_CORE__sampleRate);
     auto const count = p.get_num_ports();
     minValue.resize(count); maxValue.resize(count); defValue.resize(count);
-    value.resize(count);
     p.get_port_ranges_float(&minValue.front(), &maxValue.front(), &defValue.front());
     for (int i = 0; i < count; i++) {
       if (p.get_port_by_index(i).has_property(lv2_core__sampleRate)) {
         minValue[i] *= bela->audioSampleRate;
         maxValue[i] *= bela->audioSampleRate;
       }
-      value[i] = defValue[i];
     }
+    value = defValue;
   }
   void activate() override { instance->activate(); }
   void deactivate() override { instance->deactivate(); }
@@ -65,6 +84,7 @@ private:
 
 class VocProc final : public LV2Plugin {
 public:
+  static constexpr const char *uri = "http://hyperglitch.com/dev/VocProc";
   VocProc(BelaContext *bela, Lilv::World &lilv, Lilv::Plugin p)
   : LV2Plugin(bela, lilv, p) {
     auto const count = p.get_num_ports();
@@ -84,20 +104,11 @@ public:
     }
   }
   void run(BelaContext *bela) override {
-    float analog[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-    for (int frame = 0; frame < bela->analogFrames; frame++) {
-      for (int channel = 0; channel < bela->analogInChannels; channel++) {
-        analog[channel] += analogReadNI(bela, frame, channel);
-      }
-    }
-    for (int channel = 0; channel < bela->analogInChannels; channel++) {
-      analog[channel] /= bela->analogFrames;
-    }
-    controlFromAnalog(3, analog[0]); // Pitch Factor
-    controlFromAnalog(4, analog[1]); // Robotize/Whisperize
-    controlFromAnalog(8, analog[2]); // Threshold
-    controlFromAnalog(9, analog[3]); // Attack
-    controlFromAnalog(10, analog[4]); // Transpose
+    controlFromAnalog(3, analogReadNI(bela, 0, 0)); // Pitch Factor
+    controlFromAnalog(4, analogReadNI(bela, 0, 1)); // Pitch Factor); // Robotize/Whisperize
+    controlFromAnalog(8, analogReadNI(bela, 0, 2)); // Pitch Factor); // Threshold
+    controlFromAnalog(9, analogReadNI(bela, 0, 3)); // Pitch Factor); // Attack
+    controlFromAnalog(10, analogReadNI(bela, 0, 4)); // Pitch Factor); // Transpose
 
     instance->run(bela->audioFrames);
 
@@ -111,15 +122,20 @@ class pepper {
   Lilv::World lilv;
   std::vector<std::unique_ptr<Mode>> plugins;
   int index = -1;
+  void do_task() {
+    return;
+  }
+  MemFun<NonRT, pepper> task;
 public:
-  pepper(BelaContext *bela) {
+  pepper(BelaContext *bela) : task("task", &pepper::do_task, this) {
     lilv.load_all();
     auto lv2plugins = lilv.get_all_plugins();
     for (auto i = lv2plugins.begin(); !lv2plugins.is_end(i); i = lv2plugins.next(i)) {
       auto lv2plugin = Lilv::Plugin(lv2plugins.get(i));
       auto name = Lilv::Node(lv2plugin.get_name());
       std::cout << "Seen " << name.as_string() << std::endl;
-      if (name.is_string() && name.as_string() == std::string("VocProc")) {
+      auto uri = Lilv::Node(lv2plugin.get_uri());
+      if (uri.is_uri() && strcmp(uri.as_string(), VocProc::uri) == 0) {
         plugins.emplace_back(new VocProc(bela, lilv, lv2plugin));
         std::cout << "Loaded " << name.as_string() << std::endl;
       }
@@ -129,6 +145,7 @@ public:
   }
   ~pepper() { for (auto &plugin: plugins) plugin->deactivate(); }
   void run(BelaContext *bela) {
+    task();
     if (index >= 0 && index < plugins.size()) {
       plugins[index]->run(bela);
     }
@@ -137,30 +154,25 @@ public:
 
 // Bela
 
-std::unique_ptr<pepper> p;
+static std::unique_ptr<pepper> p;
 
-void Bela_userSettings(BelaInitSettings *settings)
-{
+void Bela_userSettings(BelaInitSettings *settings) {
   settings->interleave = 0;
 }
 
-bool setup(BelaContext *context, void *userData)
-{
+bool setup(BelaContext *context, void *userData) {
   if ((context->flags & BELA_FLAG_INTERLEAVED) == BELA_FLAG_INTERLEAVED) {
-    std::cerr << "This project only works in non-interleaved mode, "
-              << "a custom main is required." << std::endl;
+    std::cerr << "This project only works in non-interleaved mode." << std::endl;
     return false;
   }
   p = std::unique_ptr<pepper>(new pepper(context));
   return true;
 }
 
-void render(BelaContext *context, void *userData)
-{
+void render(BelaContext *context, void *) {
   p->run(context);
 }
 
-void cleanup(BelaContext *context, void *userData)
-{
+void cleanup(BelaContext *context, void *userData) {
   p = nullptr;
 }
