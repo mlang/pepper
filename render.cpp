73b25e90a890725e675fc1fe102a81a98f09182c
Mario Lang <mlang@delysid.org>
2018-06-09 15:52:25 +0200

    Boost license

--- render.cpp
+++ render.cpp
@@ -1,3 +1,7 @@
+// Copyright Mario Lang, 2018
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt)
 #include "AuxTask.h"
 #include "EdgeDetect.h"
 #include "RTPipe.h"
